36de4b121be413f0c27b06bb309930a37e1b8b7b
Pradeep107 <31689606+Pradeep107@users.noreply.github.com>
2018-02-15 16:14:05 +0530

    Signin updated

---
+++ src/ligin.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
+/**
+ * Servlet implementation class LoginController
+ */
+public class ligin extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String un=request.getParameter("username");
+		String pw=request.getParameter("password");
+		System.out.print(un);
+		
+		if(un.equals("admin") && pw.equals("admin"))
+		{
+			response.sendRedirect("success.html");
+			return;
+		}
+		else
+		{
+			response.sendRedirect("error.html");
+			return;
+		}
+	}
+}
